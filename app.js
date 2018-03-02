6015ec183da888fb5ed1e3089cd8833e33c1bb36
sakkuru <sakkuru0323@gmail.com>
2018-03-02 07:44:00 +0000

    update app.js

--- app.js
+++ app.js
@@ -77,7 +77,7 @@ const desiredHandler = (version, desired) => {
         delay = Number(desired.delay);
         sendSensorData(delay);
     }
-}
+};
 
 const sendMessage = message => {
     if (typeof message != 'string') message = JSON.stringify(message);
@@ -97,13 +97,40 @@ const sendReportProperty = message => {
     client.publish(pubTwinReportedTopic + '1', JSON.stringify(reported));
 };
 
+const formatData = raw => {
+    const data = {};
+    data.accelX = raw.accel.x;
+    data.accelY = raw.accel.y;
+    data.accelZ = raw.accel.z;
+    data.temperature = raw.temperature;
+    data.humidity = raw.humidity;
+    data.time = raw.timestamp;
+
+    return data;
+}
+
 const sendSensorData = delay => {
     sendDataTimer = setInterval(() => {
-        getSensorData().then(sensorData => {
+        getSensorData().then(rawData => {
             // console.log('sensorData', sensorData);
+            const sensorData = formatData(rawData);
             sendMessage(JSON.stringify(sensorData));
         });
     }, delay);
+};
+
+const getIpAdresses = () => {
+    const ifs = os.networkInterfaces();
+    const ipAddress = {
+        ip: {}
+    };
+    for (let i in ifs) {
+        if (!ipAddress.ip[i]) ipAddress.ip[i] = [];
+        ifs[i].forEach(info => {
+            ipAddress.ip[i].push(info.address);
+        });
+    }
+    return ipAddress;
 }
 
 client.on('connect', () => {
@@ -113,17 +140,12 @@ client.on('connect', () => {
     client.subscribe(subMethodTopic);
 });
 
-sendSensorData(delay);
-sense.showMessage("Connecting Azure IoT with MQTT.", 0.1, [255, 0, 0]);
-const ifs = os.networkInterfaces();
+/////////////////////////////////////////////////////////////////////
 
-const ipAddress = {};
-for (let i in ifs) {
-    ifs[i][0];
-    ipAddress[i] = ifs[i][0].address;
-}
-console.log(ipAddress);
-sendMessage(ipAddress);
+sendSensorData(delay);
+sense.showMessage("Connecting Azure IoT Hub with MQTT!", 0.1, [255, 255, 255], [50, 50, 50]);
+
+sendMessage(getIpAdresses());
 
 const O = [0, 0, 0];
 const X = [255, 0, 0];
